a91f197a464576e9d81aa44c15ee0d1fcfdddaef
Sergei Brykalov <lastsebr@gmail.com>
2026-02-18 17:04:09 +0300

    fix layout

--- frontend/src/app/[user]/page.tsx
+++ frontend/src/app/[user]/page.tsx
@@ -1,4 +1,6 @@
+import Link from 'next/link'
 import { notFound } from 'next/navigation'
+import { data as headerData } from '@/components/header/data'
 import IconButton from '@/components/ui/buttons/icon-button'
 import { getSessionUser } from '@/services/auth'
 import { getPinterestPins } from '@/services/pinterest-pins'
@@ -21,12 +23,18 @@ export default async function UserPage({ params }: PageProps) {
 
    const username = decodeURIComponent(session.user)
    const pinsResponse = await getPinterestPins()
-   const pins = pinsResponse?.items ?? []
+   const { items: pins = [] } = pinsResponse ?? {}
+   const { logo } = headerData
 
    return (
       <main className={styles.account}>
          <div className={styles['account-header']}>
-            <IconButton href="/" variant="back" />
+            <div className={styles['account-header__left']}>
+               <IconButton href="/" variant="back" />
+               <Link href="/" className={styles['account-header__logo']}>
+                  {logo}
+               </Link>
+            </div>
             <UserInfo username={username} profileImageUrl={session.profileImageUrl} />
          </div>
          <PinsList pins={pins} />
